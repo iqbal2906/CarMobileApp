463e83c355fb72be2bcec80d415ca3216ddf6a7c
iqbal2906 <maniacjoe3@gmail.com>
2021-04-12 22:33:28 +0700

    add background image

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import Colors from "./constants/Colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.carContainer}>
+        <ImageBackground
+          source={require("./assets/images/ModelX.jpeg")}
+          style={styles.image}
+        />
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subTitle}>Starting at $69,420</Text>
@@ -39,5 +43,11 @@ const styles = StyleSheet.create({
   subTitle: {
     fontSize: 16,
     color: Colors.subTitle,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
   },
 });
